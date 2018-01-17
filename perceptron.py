b6c7bdfd5eb5ed686949334ab3124c4f6241be36
ArchibaldLeMagnifique <denis.lachartre@free.fr>
2018-01-17 16:50:15 +0100

    normalize

--- perceptron.py
+++ perceptron.py
@@ -8,39 +8,48 @@ Created on Wed Jan 17 09:22:39 2018
 import numpy as np
 from random import randint
 
-def readData (path, nbApprentissage, prediction):
-    file = open("./dataset/"+path, "r")
+def normalize (set):
+    length = len(set[0].strip("\n").split(','))-1
+    for k in range(length):
+        for i in range(len(data)):
+            1
+
+def getDataIris (proportion):
+    file = open("./dataset/iris.data", "r")
     
-    X=[]
-    Y=[]
+    lines = normalize(file.readlines())
+
+    baseApprentissage = []
+    baseTest = []
     
-    for k in range(nbApprentissage):
-        line = file.readline().split(',')
-        X.append(line[:-1])
-        if (line[-1].rstrip() == prediction):
-            Y.append(1)
+    for k in range(len(lines)):
+        strline = lines[k].strip("\n").split(',')
+        line = list(map(float, strline[:-1]))
+        
+        if (k < len(lines) * proportion):
+            if (strline[-1] == "Iris-setosa"):
+                baseApprentissage.append(line + [1.0])
+            else:
+                baseApprentissage.append(line + [-1.0])
         else:
-            Y.append(-1)
-    return (X,Y)
+            if (strline[-1] == "Iris-setosa"):
+                baseTest.append(line + [1.0])
+            else:
+                baseTest.append(line + [-1.0])
+    return (baseApprentissage, baseTest)
+
 
 def perceptron (X, Y, eta, T):
     t=0
-    nbElem = len(X[0])
-    
-    #initialisation du vecteur poid
-    w = np.zeros(nbElem)
-    
+    nbElem = len(X[0])    
+    w = np.zeros(nbElem)    
     while (t<T):
         i = randint(0, len(X)-1)  
         
         prodScal = 0
         for j in range(nbElem):
-            print("i : {}".format(i))
-            print ("j : {}".format(j))
-            print ("w[j]*X[i][j] : {}".format(w[j]*float(X[i][j])))
             prodScal = prodScal + w[j]*float(X[i][j])
 
-        print("Produit scalaire : {}".format(prodScal))
         if (Y[i] * prodScal <= 0):
             w[0] = w[0] + eta * Y[i]
             for j in range(nbElem-1):
@@ -48,10 +57,26 @@ def perceptron (X, Y, eta, T):
         t = t+1
     return(w)
 
+def testMethode(baseTest, w) :
+    size_of_space = len(baseTest[0]) -1
+    perf = 0.0
+    for i in range(len(baseTest)) :
+        yi = baseTest[i][size_of_space]
+        xi = [1] + list(baseTest[i][0:size_of_space])
+        if (yi * np.dot(w, xi)) > 0 :
+            perf += 1.0
+    return perf/float(len(baseTest))
+
 
 eta = 0.2
 T = 100
 
-(X,Y) = readData('iris.txt', 100, 'Iris-setosa')
+(baseApprentissage,baseTest) = getDataIris(1/3)
 
-moulis = perceptron(X, Y, eta, T)
+#w = perceptron(X, Y, eta, T)
+
+
+
+
+
+
