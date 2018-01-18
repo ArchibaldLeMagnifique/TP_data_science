337146b2e4561230a53138f5ac2f5879ccf33515
ArchibaldLeMagnifique <denis.lachartre@free.fr>
2018-01-18 17:28:50 +0100

    tig

--- perceptron.py
+++ perceptron.py
@@ -8,75 +8,93 @@ Created on Wed Jan 17 09:22:39 2018
 import numpy as np
 from random import randint
 
-def normalize (set):
+def parseAndNormalize (set):
     length = len(set[0].strip("\n").split(','))-1
-    for k in range(length):
-        for i in range(len(data)):
-            1
-
-def getDataIris (proportion):
-    file = open("./dataset/iris.data", "r")
     
-    lines = normalize(file.readlines())
+    res = [[] for _ in range(len(set))]
+    
+    for k in range(length):
+        vmax = float(set[0].strip("\n").split(',')[k])
+        vmin = vmax
+        for i in range(len(set)):
+            vmax = max(vmax, float(set[i].strip("\n").split(',')[k]))
+            vmin = min(vmin, float(set[i].strip("\n").split(',')[k]))
+        for i in range(len(set)):
+            res[i].append((float(set[i].strip("\n").split(',')[k]) - vmin)/(vmax - vmin))
+    for i in range(len(set)):
+        res[i].append(set[i].strip("\n").split(',')[-1])
+    return res
 
-    baseApprentissage = []
-    baseTest = []
+def getDataIris ():
+    file = open("./dataset/iris.data", "r")
+    lines = parseAndNormalize(file.readlines())
+    base = []
     
     for k in range(len(lines)):
-        strline = lines[k].strip("\n").split(',')
+        strline = lines[k]
         line = list(map(float, strline[:-1]))
-        
-        if (k < len(lines) * proportion):
-            if (strline[-1] == "Iris-setosa"):
-                baseApprentissage.append(line + [1.0])
-            else:
-                baseApprentissage.append(line + [-1.0])
+        if (strline[-1] == "Iris-setosa"):
+            base.append(line + [1.0])
         else:
-            if (strline[-1] == "Iris-setosa"):
-                baseTest.append(line + [1.0])
-            else:
-                baseTest.append(line + [-1.0])
-    return (baseApprentissage, baseTest)
+            base.append(line + [-1.0])
+    return base
 
+# k ieme partition de la base decoupe en K, et le reste
+def splitBase(base, k, K):
+    base1 = []
+    base2 = []
+    for i in range(len(base)):
+        if (i>((k-1)/K*len(base)) and i<(k/K*len(base))):
+            base1.append(base[i])
+        else:
+            base1.append(base[i])
+    return (base, base2)
 
-def perceptron (X, Y, eta, T):
+def perceptron (base, eta, T):
     t=0
-    nbElem = len(X[0])    
-    w = np.zeros(nbElem)    
+    
+    nbElem = len(base[0]) - 1
+    w0 = 0.0
+    w = np.zeros(nbElem)
     while (t<T):
-        i = randint(0, len(X)-1)  
-        
-        prodScal = 0
-        for j in range(nbElem):
-            prodScal = prodScal + w[j]*float(X[i][j])
+        i = randint(0, len(base)-1)
+        prodScal = w0 + np.dot(w, base[i][:-1])
 
-        if (Y[i] * prodScal <= 0):
-            w[0] = w[0] + eta * Y[i]
-            for j in range(nbElem-1):
-                w[j+1] = w[j] + eta * Y[i] * float(X[i][j])
+        if (base[i][-1] * prodScal <= 0):
+            w0 = w0 + eta * base[i][-1]
+            for j in range(nbElem):
+                w[j] = w[j] + eta * base[i][-1] * base[i][j]
         t = t+1
-    return(w)
-
-def testMethode(baseTest, w) :
-    size_of_space = len(baseTest[0]) -1
-    perf = 0.0
-    for i in range(len(baseTest)) :
-        yi = baseTest[i][size_of_space]
-        xi = [1] + list(baseTest[i][0:size_of_space])
-        if (yi * np.dot(w, xi)) > 0 :
-            perf += 1.0
-    return perf/float(len(baseTest))
+    return (w0, w)
 
 
-eta = 0.2
-T = 100
+def testPoids(base, w0, w):
+    perf = 0
+    for i in range(len(base)):
+        y = base[i][-1]
+        x = base[i][:-1]
+                
+        if (y * np.dot(w, x) + w0) > 0 :
+            perf = perf + 1
+    return perf/float(len(base))
 
-(baseApprentissage,baseTest) = getDataIris(1/3)
 
-#w = perceptron(X, Y, eta, T)
+# on divise base A et on fait une cross validation
+def erreurMoyenne(eta, T, baseA):
+    erreur = 0
+    for k in range(K):        
+        (baseEntrainement, baseValidation) = splitEV(base, k, K)
+        (w0 ,w) = perceptron(baseA, eta, T)
+        erreur = erreur + testPoids(baseA, w0, w)
+    return (erreur/K)
+    
 
+# 
+def choixEta(etas, T, baseA):
+    1
 
-
-
-
+etas = [0.001, 0.01, 0.1, 1, 10, 100, 1000]
+T = 10
+K = 5
+base = getDataIris()
 
